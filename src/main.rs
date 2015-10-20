97c58e4516ecaafce3998a1b047a3b7407758776
Andrew Jeddeloh <andrewplayssax@gmail.com>
2015-10-19 23:38:31 -0700

    Major refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,72 @@
-extern crate ncurses;
+extern crate rustbox;
 
-use std::char;
 use std::io::{BufReader, BufRead, Write};
 use std::net::TcpStream;
 
-use ncurses::*;
+use rustbox::{Color, RustBox, Event, Key};
 
 #[derive(Debug)]
-struct MPDSearch {
+struct Constraint {
     search_type: String,
     search_term: String,
 }
 
-impl MPDSearch {
+impl Constraint {
+    pub fn new(srch_type: String ) -> Constraint {
+        Constraint {search_type: srch_type, search_term: String::new() }
+    }
+
     fn to_mpd_string(&self) -> String {
         format!("{} \"{}\" ", self.search_type, self.search_term)
+    }
+
+    fn to_display_string(&self) -> String {
+        format!("{}: {}", self.search_type, self.search_term)
+    }
+}
+
+struct MPC {
+    connection: TcpStream,
+    reader: BufReader<TcpStream>,
+}
+
+impl MPC {
+    pub fn new() -> MPC {
+        let mut buf = String::new();
+        let write_conn = match TcpStream::connect("localhost:6600") {
+            Ok(v) => v,
+            Err(e) => panic!("Could not connect to MPD.\n{}", e)
+        };
+        let read_conn = match write_conn.try_clone() {
+            Ok(v) => v,
+            Err(e) => panic!("Could not clone MPD connection for reading\n{}", e)
+        };
+        //intentionally transferring ownership
+        let mut reader = BufReader::new(read_conn);
+        reader.read_line(&mut buf).unwrap();
+        assert!(buf.starts_with("OK"), "MPD connected, but did not return OK upon connection");
+        MPC {
+            connection: write_conn,
+            reader: reader
+        }
+    }
+
+    fn send_command(&mut self, command: &str) -> Vec<String> {
+        self.connection.write(command.as_bytes()).ok().expect("Error writing to MPD");
+        self.connection.write("\n".as_bytes()).ok().expect("Error writing to MPD");
+        let mut results: Vec<String> = Vec::new();
+        let mut buf = String::new();
+        loop {
+            buf.clear();
+            self.reader.read_line(&mut buf).ok().expect("Error reading from MPD");
+            if buf == "OK\n" {
+                return results;
+            } else if buf.starts_with("ACK") {
+                panic!("MPD returned an ACK for command {}", command);
+            } else {
+                results.push(String::from(buf.trim()));
+            }
+        }
     }
 }
 
@@ -29,143 +81,147 @@ fn char_to_mpd_type(c: char) -> Option<String> {
         'A' => Some("albumartist"),
         '\n' => Some(""), //end of input
         _ => None
-    }.map(|x| x.to_string() )
+    }.map(|x| x.to_string())
 }
 
-fn is_valid_char(x: char) -> bool {
-    x.is_alphanumeric() || x.is_whitespace()
-}
-
-fn get_valid_char() -> char {
-    let c = char::from_u32(getch() as u32);
-    match c {
-        Some (x) if is_valid_char(x) => x,
-        Some (_) => get_valid_char(),
-        None => get_valid_char(),
-    }
-}
-
-fn get_valid_type() -> String {
-    match char_to_mpd_type(get_valid_char()) {
-        Some(x) => x,
-        None => get_valid_type(),
-    }
-}
-
-fn do_mpd_update(constraints: &Vec<MPDSearch>, conn: &mut TcpStream, reader: &mut BufReader<TcpStream>) -> Vec<String> {
-    let mut search_string = String::from("search ");
-    for constraint in constraints {
-        if constraint.search_term.len() > 0 {
-            search_string.push_str(&constraint.to_mpd_string());
-        }
-    }
-    search_string.push('\n');
-    conn.write(search_string.as_bytes()).ok().expect("DEATH AND SUFFERING");
-    let mut matched_tracks: Vec<String> = Vec::new();
-    let mut buffer = String::new();
-    while !buffer.starts_with("OK") && !buffer.starts_with("ACK") {
-        buffer.clear();
-        reader.read_line(&mut buffer).ok().expect("Failed to read from mpd socket");
-        if buffer.starts_with("file: ") {
-            matched_tracks.push(String::from(&buffer.trim()[6..]));
-        }
-    }
-    matched_tracks
-}
-
-fn add_tracks(constraints: &Vec<MPDSearch>, conn: &mut TcpStream, reader: &mut BufReader<TcpStream>) {
-    let mut buffer = String::new();
-    let mut index = -1;
-    conn.write("status\n".as_bytes()).ok().expect("PLAGUE! Hi Y$");
-    while !buffer.starts_with("OK") && !buffer.starts_with("ACK") {
-        buffer.clear();
-        reader.read_line(&mut buffer).ok().expect("Failed to read from mpd socket");
-        if buffer.starts_with("playlistlength: ") {
-            index = buffer.trim()[16..].parse().ok().expect("Server returned invalid index");
-        }
-    }
-    println!("{}", index);
-
-    //build search string 
-    let mut search_string = String::from("searchadd ");
-    for constraint in constraints {
-        if constraint.search_term.len() > 0 {
-            search_string.push_str(&constraint.to_mpd_string());
-        }
-    }
-    search_string.push('\n');
-
-    conn.write(search_string.as_bytes()).ok().expect("DEATH AND SUFFERING");
-    let mut matched_tracks: Vec<String> = Vec::new();
-    while !buffer.starts_with("OK") && !buffer.starts_with("ACK") {
-        buffer.clear();
-        reader.read_line(&mut buffer).ok().expect("Failed to read from mpd socket");
-        if buffer.starts_with("file: ") {
-            matched_tracks.push(String::from(&buffer.trim()[6..]));
-        }
-    }
-
-    conn.write(format!("play {}\n",index).as_bytes()).ok().expect("Its 2am");
-    buffer.clear();
-    reader.read_line(&mut buffer).ok().expect("Error reading mpd");
-    assert!(buffer.starts_with("OK"));
-
-}
-
-fn print_screen(constraints: &Vec<MPDSearch>, files: &Vec<String>) {
-    wclear(stdscr);
-    addstr("Filters:\n");
-    for constraint in constraints {
-        addstr(&constraint.to_mpd_string());
-        addch('\n' as u64);
-    }
-    for file in files {
-        addstr(&file);
-        addch('\n' as u64);
-    }
-}
-
-fn main() {
-    //connect to the mpd socket
-    let read_conn = TcpStream::connect("127.0.0.1:6600").unwrap();
-    let mut write_conn = read_conn.try_clone().ok().expect("ERROR");
-    let mut reader = BufReader::new(read_conn);
-    let mut buf = String::new();
-    reader.read_line(&mut buf)
-        .ok()
-        .expect("Error reading line from MPD server");
-    assert!( buf.starts_with("OK"), "Server did not return OK");
-
-    //setup ncurses
-    initscr();
-    noecho();
-    printw("Filters:\n");
-    refresh();
+fn print_screen(constraints: &Vec<Constraint>, files: &Vec<String>, rustbox: &RustBox) {
+    let mut y = 1;
+    let height = rustbox.height();
+    rustbox.clear();
+    rustbox.print(0, 0, rustbox::RB_BOLD, Color::Default, Color::Default, "Filters:");
     
-    //create initial set of search constraints
-    let mut constraints: Vec<MPDSearch> = Vec::new();
-    let mut matched_tracks: Vec<String> = Vec::new();
-
-    loop {
-        //do_mpd_update(&constraints, &mut write_conn, &mut reader);
-        print_screen(&constraints, &matched_tracks);
-        let current_search = MPDSearch { search_type: get_valid_type(), search_term: String::new() };
-        if current_search.search_type == "" {
-            break;
+    for constraint in constraints {
+        rustbox.print(0, y, rustbox::RB_BOLD, Color::Default, Color::Default, &constraint.to_display_string());
+        y += 1;
+        if y == (height - 1) {
+            rustbox.present();
+            return;
         }
-        constraints.push(current_search);
-        print_screen(&constraints, &matched_tracks);
-
-        
-        let mut c = get_valid_char();
-        while c != '\n' {
-            constraints.last_mut().unwrap().search_term.push(c);
-            matched_tracks = do_mpd_update(&constraints, &mut write_conn, &mut reader);
-            print_screen(&constraints, &matched_tracks);
-            c = get_valid_char();
+    }
+    
+    for file in files {
+        rustbox.print(0, y, rustbox::RB_BOLD, Color::Default, Color::Default, &file);
+        y += 1;
+        if y == (height - 1) {
+            rustbox.present();
+            return;
         }
-    }   
-    endwin();
-    add_tracks(&constraints, &mut write_conn, &mut reader);   
-    println!("Added {} songs", matched_tracks.len());
+    }
+    rustbox.present();
+}
+
+enum State {
+    NeedType,
+    NeedString,
+    ShouldExit,
+    ShouldCommit
+}
+
+impl State {
+    fn is_exit_state(&self) -> bool {
+        match *self {
+            State::ShouldExit => true,
+            State::ShouldCommit => true,
+            _ => false
+        }
+    }
+}
+fn main() {
+    let mut mpc = MPC::new();
+
+    let mut constraints: Vec<Constraint> = Vec::new();
+    let mut state = State::NeedType;
+
+    {
+        //using rustbox now
+        let rustbox = RustBox::init(Default::default()).ok().expect("Error initializing rustbox");
+        while !state.is_exit_state() {
+            //get update from mpd
+            let matched_files = match constraints.is_empty() {
+                true => Vec::new(),
+                false => {
+                    let mut query = String::from("search ");
+                    for constraint in &constraints {
+                        query.push_str(&constraint.to_mpd_string());
+                    };
+                    mpc.send_command(&query).into_iter().filter(|x| x.starts_with("file")).collect()
+                }
+            };
+            
+            //update display
+            print_screen(&constraints, &matched_files, &rustbox);
+
+            //get input
+            let key = match rustbox.poll_event(false) {
+                Ok(Event::KeyEvent(key)) => key,
+                Ok(_) => None,
+                Err(e) => panic!("Error with rustbox {}",e)
+            };
+
+            //ungodly state transition matrix
+            state = match (state, key) {
+                //handle all cases we should never reach
+                (State::ShouldExit,   _) => panic!("unreachable code reached!"),
+                (State::ShouldCommit, _) => panic!("unreachable code reached!"),
+                //handle all cases where we got no input
+                (State::NeedType,     None) => State::NeedType,
+                (State::NeedString,   None) => State::NeedString,
+                (_, Some(Key::Esc)) => State::ShouldExit,
+                //handle all cases for NeedType
+                (State::NeedType, Some(Key::Enter)) => State::ShouldCommit,
+                (State::NeedType, Some(Key::Backspace)) => match constraints.is_empty() {
+                        true  => State::NeedType, //lready at the end
+                        false => {
+                                constraints.pop();
+                                State::NeedString
+                            }
+                    },
+                (State::NeedType, Some(Key::Char(x))) => match char_to_mpd_type(x) {
+                        Some(str) => {
+                                constraints.push(Constraint::new(str));
+                                State::NeedString
+                            },
+                        None => State::NeedType
+                    },
+                (State::NeedType, _) => State::NeedType,
+                //
+                (State::NeedString, Some(Key::Enter)) => State::NeedType,
+                (State::NeedString, Some(Key::Backspace)) => match constraints.is_empty() {
+                        false => {
+                                constraints.last_mut().unwrap().search_term.pop();
+                                State::NeedString
+                            },
+                        true => State::NeedType
+                    },
+                (State::NeedString, Some(Key::Char(x))) if x.is_alphanumeric() || x.is_whitespace() => {
+                        constraints.last_mut().unwrap().search_term.push(x);
+                        State::NeedString
+                    },
+                (State::NeedString, _) => State::NeedString
+            };
+        }
+    }//end using rustbox
+
+    //get the current playlist length
+    let last_pos_str = mpc.send_command("status")
+        .into_iter()
+        .find(|x| x.starts_with("playlistlength: "))
+        .unwrap();
+    let last_pos = last_pos_str
+        .split(' ')
+        .last()
+        .unwrap();
+    println!("{}", last_pos);
+    if constraints.is_empty() {
+        return ;
+    }
+    let mut query = String::from("searchadd ");
+    for constraint in &constraints {
+        query.push_str(&constraint.to_mpd_string());
+    };
+    mpc.send_command(&query);
+    query = String::from("play ");
+    query.push_str(last_pos);
+    mpc.send_command(&query);
+
 }
